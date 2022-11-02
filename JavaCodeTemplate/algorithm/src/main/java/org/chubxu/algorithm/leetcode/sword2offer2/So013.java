2affd30178f738f4ebc0339f43ae3583c774d0f3
chubxu <chubxu@gmail.com>
2022-11-02 22:59:00 +0800

    feat: so013

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/sword2offer2/So013.java
@@ -0,0 +1,69 @@
+package org.chubxu.algorithm.leetcode.sword2offer2;
+
+/**
+ * @ClassName So013
+ * @Description
+ *
+ * 给定一个二维矩阵 matrix，以下类型的多个请求：
+ *
+ * 计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+ * 实现 NumMatrix 类：
+ *
+ * NumMatrix(int[][] matrix) 给定整数矩阵 matrix 进行初始化
+ * int sumRegion(int row1, int col1, int row2, int col2) 返回左上角 (row1, col1) 、右下角 (row2, col2) 的子矩阵的元素总和。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入:
+ * ["NumMatrix","sumRegion","sumRegion","sumRegion"]
+ * [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+ * 输出:
+ * [null, 8, 11, 12]
+ *
+ * 解释:
+ * NumMatrix numMatrix = new NumMatrix([[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]]);
+ * numMatrix.sumRegion(2, 1, 4, 3); // return 8 (红色矩形框的元素总和)
+ * numMatrix.sumRegion(1, 1, 2, 2); // return 11 (绿色矩形框的元素总和)
+ * numMatrix.sumRegion(1, 2, 2, 4); // return 12 (蓝色矩形框的元素总和)
+ *
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 200
+ * -105 <= matrix[i][j] <= 105
+ * 0 <= row1 <= row2 < m
+ * 0 <= col1 <= col2 < n
+ * 最多调用 104 次 sumRegion 方法
+ *
+ *
+ * 注意：本题与主站 304 题相同： https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ *
+ * @Since 1.0.0
+ * @Date 2022/11/2 22:53
+ * @Author chubxu
+ */
+public class So013 {
+}
+
+class NumMatrix {
+    private int[][] preMatrix;
+    public NumMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        preMatrix = new int[row+1][col+1];
+        for (int i = 1; i<=row; i++) {
+            for (int j = 1; j<=col; j++) {
+                preMatrix[i][j] = preMatrix[i-1][j] + preMatrix[i][j-1] - preMatrix[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return preMatrix[row2+1][col2+1] - preMatrix[row1][col2+1] - preMatrix[row2+1][col1] + preMatrix[row1][col1];
+    }
+}
