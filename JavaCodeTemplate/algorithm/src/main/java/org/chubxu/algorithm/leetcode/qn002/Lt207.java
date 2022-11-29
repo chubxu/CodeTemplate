baffb96faa0fb19259c8075168859a13f1b5cbb9
chubxu <chubxu@gmail.com>
2022-11-29 22:10:42 +0800

    feat: Lt207

--- JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn002/Lt207.java
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn002/Lt207.java
@@ -1,11 +1,79 @@
 package org.chubxu.algorithm.leetcode.qn002;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ClassName Lt207
- * @Description TODO
+ * @Description
+ *
+ * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+ *
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+ *
+ * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：numCourses = 2, prerequisites = [[1,0]]
+ * 输出：true
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+ * 示例 2：
+ *
+ * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * 输出：false
+ * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+ *
+ *
+ * 提示：
+ *
+ * 1 <= numCourses <= 105
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * prerequisites[i] 中的所有课程对 互不相同
+ *
  * @Since 1.0.0
  * @Date 2022/11/29 22:00
  * @Author chubxu
  */
 public class Lt207 {
+    private List<List<Integer>> list;
+    private boolean canFinish = true;
+    private int[] visited;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        list = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        visited = new int[numCourses];
+
+        for (int[] prerequisite : prerequisites) {
+            list.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        for (int i = 0; i < list.size() && canFinish; i++) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return canFinish;
+    }
+
+    private void dfs(int i) {
+        visited[i] = 1;
+        for (Integer integer : list.get(i)) {
+            if (visited[integer] == 0) {
+                dfs(integer);
+            } else if (visited[integer] == 1) {
+                canFinish = false;
+                return ;
+            }
+        }
+        visited[i] = 2;
+    }
 }
