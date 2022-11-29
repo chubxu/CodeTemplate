40bec5a9541c025953ddb5ddd486565b34340afc
chubxu <chubxu@gmail.com>
2022-11-29 22:10:27 +0800

    feat: Lt207

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn002/Lt207.java
@@ -0,0 +1,11 @@
+package org.chubxu.algorithm.leetcode.qn002;
+
+/**
+ * @ClassName Lt207
+ * @Description TODO
+ * @Since 1.0.0
+ * @Date 2022/11/29 22:00
+ * @Author chubxu
+ */
+public class Lt207 {
+}
