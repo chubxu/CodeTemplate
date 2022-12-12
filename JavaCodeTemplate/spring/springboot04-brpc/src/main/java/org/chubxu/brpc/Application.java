1d6e0c090153fd7b44c80c175add93e2a35c3be2
chubxu <chubxu@gmail.com>
2022-12-12 22:12:33 +0800

    feat: springboot04-brpc工程搭建

---
+++ JavaCodeTemplate/spring/springboot04-brpc/src/main/java/org/chubxu/brpc/Application.java
@@ -0,0 +1,18 @@
+package org.chubxu.brpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Since 1.0.0
+ * @Date 2022/12/12 22:06
+ * @Author chubxu
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
