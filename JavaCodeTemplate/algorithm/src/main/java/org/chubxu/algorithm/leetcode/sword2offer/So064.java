7237c202a14985da0c943d39cab4ee84442e05fa
chubxu <chubxu@gmail.com>
2023-03-20 23:05:41 +0800

    feat: So064

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/sword2offer/So064.java
@@ -0,0 +1,8 @@
+package org.chubxu.algorithm.leetcode.sword2offer;
+
+public class So064 {
+    public int sumNums(int n) {
+        boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
+        return n;
+    }
+}
