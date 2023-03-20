a1a703292f0f75157b165e374855605ff18b3029
chubxu <chubxu@gmail.com>
2023-03-21 00:04:02 +0800

    feat: So005

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/sword2offer/So005.java
@@ -0,0 +1,15 @@
+package org.chubxu.algorithm.leetcode.sword2offer;
+
+public class So005 {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
