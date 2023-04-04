4bceb71577ce1534344c80ef264c96c1ed495a7c
chubxu <chubxu@gmail.com>
2023-04-04 21:20:34 +0800

    feat: so014

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/sword2offer/So014.java
@@ -0,0 +1,17 @@
+package org.chubxu.algorithm.leetcode.sword2offer;
+
+public class So014 {
+    public int cuttingRope(int n) {
+        if (n <= 3) {
+            return n-1;
+        }
+        int a = n / 3, b = n % 3;
+        if (b == 0) {
+            return (int) Math.pow(3, a);
+        } else if (b == 1) {
+            return (int) Math.pow(3, a-1) * 4;
+        } else {
+            return (int) Math.pow(3, a) * b;
+        }
+    }
+}
