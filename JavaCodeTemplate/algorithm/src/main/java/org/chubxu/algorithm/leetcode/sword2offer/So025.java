f5cf8027578ee53e65d81d2e7ccd760f79f414fb
chubxu <chubxu@gmail.com>
2023-04-06 21:52:00 +0800

    feat: so025

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/sword2offer/So025.java
@@ -0,0 +1,27 @@
+package org.chubxu.algorithm.leetcode.sword2offer;
+
+import org.chubxu.algorithm.leetcode.ListNode;
+
+public class So025 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
+    }
+}
