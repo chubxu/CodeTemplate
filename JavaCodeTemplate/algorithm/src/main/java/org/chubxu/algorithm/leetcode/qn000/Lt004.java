051ad80de5facb1029f9e1422067138d7a16d5b6
chubxu <chubxu@gmail.com>
2023-04-13 22:03:05 +0800

    feat: lt004

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn000/Lt004.java
@@ -0,0 +1,44 @@
+package org.chubxu.algorithm.leetcode.qn000;
+
+public class Lt004 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int totalLen = len1 + len2;
+        if (totalLen % 2 == 1) {
+            int midIndex = totalLen / 2;
+            return help(nums1, nums2, midIndex+1);
+        } else {
+            int midIndex1 = totalLen / 2 - 1, midIndex2 = totalLen / 2;
+            return (help(nums1, nums2, midIndex1+1) + help(nums1, nums2, midIndex2 + 1)) / 2.0;
+        }
+    }
+
+    public double help(int[] nums1, int[] nums2, int k) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int index1 = 0, index2 = 0;
+
+        while (true) {
+            if (index1 == len1) {
+                return nums2[index2 + k - 1];
+            }
+            if (index2 == len2) {
+                return nums1[index1 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, len1) - 1;
+            int newIndex2 = Math.min(index2 + half, len2) - 1;
+            if (nums1[newIndex1] <= nums2[newIndex2]) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
+        }
+    }
+
+}
