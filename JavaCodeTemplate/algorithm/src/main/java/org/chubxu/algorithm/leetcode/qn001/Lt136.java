b3aefe42d878342988895764b797d76d39fee381
chubxu <chubxu@gmail.com>
2023-04-14 20:12:28 +0800

    feat: lt136

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn001/Lt136.java
@@ -0,0 +1,11 @@
+package org.chubxu.algorithm.leetcode.qn001;
+
+public class Lt136 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
