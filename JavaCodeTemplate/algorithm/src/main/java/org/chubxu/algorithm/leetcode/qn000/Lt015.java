5885f8a047ce4783000738547b2244e4a2dd8c84
chubxu <chubxu@gmail.com>
2023-04-17 22:35:21 +0800

    feat: Lt015

--- JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn000/Lt015.java
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn000/Lt015.java
@@ -1,4 +1,36 @@
 package org.chubxu.algorithm.leetcode.qn000;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Lt015 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) return Collections.emptyList();
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i+1, r = nums.length-1;
+            while (l < r) {
+                int rt = nums[i] + nums[l] + nums[r];
+                if (rt == 0) {
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+                    l++;
+                    r--;
+                    continue;
+                }
+                if (rt < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
 }
