1d776acef8fd89b776ed51c08d056b8edf1af599
chubxu <chubxu@gmail.com>
2023-04-26 23:19:41 +0800

    add Lt605

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn006/Lt605.java
@@ -0,0 +1,17 @@
+package org.chubxu.algorithm.leetcode.qn006;
+
+public class Lt605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0, len = flowerbed.length; i < len && n > 0;) {
+            if (flowerbed[i] == 1) {
+                i += 2;
+            } else if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
+                n--;
+                i += 2;
+            } else {
+                i += 3;
+            }
+        }
+        return n <= 0;
+    }
+}
