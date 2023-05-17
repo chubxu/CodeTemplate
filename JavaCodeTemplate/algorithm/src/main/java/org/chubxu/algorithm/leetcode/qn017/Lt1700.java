e26024e5b5d38b487f76b2d3fdcafa480a6ec14c
chubxu <chubxu@gmail.com>
2023-05-17 23:20:44 +0800

    add: Lt1700

---
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn017/Lt1700.java
@@ -0,0 +1,20 @@
+package org.chubxu.algorithm.leetcode.qn017;
+
+import java.util.Arrays;
+
+public class Lt1700 {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int s1 = Arrays.stream(students).sum();
+        int s0 = students.length - s1;
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0 && s0 > 0) {
+                s0--;
+            } else if (sandwich == 1 && s1 > 0) {
+                s1--;
+            } else {
+                break;
+            }
+        }
+        return s0 + s1;
+    }
+}
