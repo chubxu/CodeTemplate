c0d9cd8a32cf3c1ca342b95dfb03b1c586381417
chubxu <chubxu@gmail.com>
2023-05-22 22:22:36 +0800

    add: lt1823

--- JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn018/Lt1823.java
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn018/Lt1823.java
@@ -1,4 +1,35 @@
 package org.chubxu.algorithm.leetcode.qn018;
 
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Lt1823 {
+    public int findTheWinner(int n, int k) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            queue.add(i+1);
+        }
+        while (queue.size() != 1) {
+            for (int i = 0; i < k-1; i++) {
+                queue.addLast(queue.pollFirst());
+            }
+            queue.pollFirst();
+        }
+        return queue.peek();
+    }
+
+    public int findTheWinner2(int n, int k) {
+        int rt = 0;
+        for (int i = 2; i<n+1; i++) {
+            rt = (rt + k) % i;
+        }
+        return rt + 1;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(findTheWinner(6, 5));
+    }
 }
