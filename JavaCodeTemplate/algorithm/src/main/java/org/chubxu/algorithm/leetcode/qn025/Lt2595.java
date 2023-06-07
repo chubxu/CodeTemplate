a3740861342815e303addbc612b1bd1e417b46d3
chubxu <chubxu@gmail.com>
2023-06-07 21:24:12 +0800

    update: lt2595

--- JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn025/Lt2595.java
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn025/Lt2595.java
@@ -12,4 +12,17 @@ public class Lt2595 {
         final int MASK = 0x5555;
         return new int[]{Integer.bitCount(n & MASK), Integer.bitCount(n & (MASK >> 1))};
     }
+
+    public int[] evenOddBit3(int n) {
+        int mask = 0x5555;
+        return new int[] {Integer.bitCount(n & mask), Integer.bitCount(n & (mask >> 1))};
+    }
+
+    public int[] evenOddBit4(int n) {
+        int[] res = new int[2];
+        for (int i = 0; n>0; i ^= 1, n >>= 1) {
+            res[i]+=(n&1);
+        }
+        return res;
+    }
 }
