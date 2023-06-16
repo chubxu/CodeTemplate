729dbc045c74621ad7fc3200aba53608bbe7c34c
chubxu <chubxu@gmail.com>
2023-06-16 22:34:57 +0800

    add: Lt2331

--- JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn023/Lt2331.java
+++ JavaCodeTemplate/algorithm/src/main/java/org/chubxu/algorithm/leetcode/qn023/Lt2331.java
@@ -1,2 +1,15 @@
-package org.chubxu.algorithm.leetcode.qn023;public class Lt2331 {
+package org.chubxu.algorithm.leetcode.qn023;
+
+import org.chubxu.algorithm.leetcode.TreeNode;
+
+public class Lt2331 {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val == 0 || root.val == 1) {
+            return root.val != 0;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
 }
